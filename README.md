16bd98ac422c90aa828c774577a829db7f24b305
n89cz <root@n89.cz>
2018-10-31 16:05:58 +0100

    small updates

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Nagios plugins
 check_process.sh - Nagios plugin that checks if docker daemon is up or down
 
 check_docker_container.sh - check container status
+
+check_ssl.sh - 
--- check_ssl.sh
+++ check_ssl.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-SERVERNAME="$1"
-
-# zjistit not after date
-# od neho odecist 5 (warning) a 2 (critical)
-# vypsat soucasny stav %s (date +%s) a porovnat
-# pokud soucasny stav mensi nez warning tak prislusna chyba, pokud soucasny stav mensi ne critical tak chyba, pokud soucasny stav vetsi nez critical i nez warning tak je vse v poradku a do expirace certifikatu zbyva vice nez 5 dni
+SRVNAME="$1"
 
 #NAGIOS return values
 NAGIOS_OK="0"
@@ -14,7 +9,7 @@ NAGIOS_CRITICAL="2"
 NAGIOS_UNKNOWN="3"
 
 
-NOT_AFTER=$(echo | openssl s_client -servername $SERVERNAME -connect $SERVERNAME:443 2>/dev/null | openssl x509 -noout -enddate | sed -e 's#notAfter=##')
+NOT_AFTER=$(echo | openssl s_client -servername $SRVNAME -connect $SRVNAME:443 2>/dev/null | openssl x509 -noout -enddate | sed -e 's#notAfter=##')
 NOT_AFTER_SEC=`date -d "${NOT_AFTER}" '+%s'`
 
 #expire intervals in seconds
@@ -36,7 +31,6 @@ elif [ "$SECONDS_NOW" -gt "$WRNSEC" ] ; then
     echo "WARNING - Five days to certificate expiration"
     exit $NAGIOS_WARNING
 fi
-
 
 
 #echo "Not after sec: "$NOT_AFTER_SEC

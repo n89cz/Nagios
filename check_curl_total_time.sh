1637ca8b3ae495707a8186d1840c1d752eb657fc
Martin Dusek <martin.dusek@wedos.org>
2019-03-11 14:53:29 +0100

    curl response total time

--- check_curl_total_time.sh
+++ check_curl_total_time.sh
@@ -3,6 +3,9 @@
 #DOMAIN=$1
 DOMAIN="https://domain.name"
 
+#
+WARN=0.7
+
 #Nagios status codes
 STATE_OK="0"
 STATE_WARNING="1"
@@ -13,7 +16,7 @@ STATE_UNKNOWN="3"
 
 RESPONSE=`curl -o /dev/null -s -w '%{time_total}' $DOMAIN | tr ',' '.'`
 
-if [[ $RESPONSE < 0.19 ]]; then
+if [[ "$RESPONSE" < "$WARN" ]]; then
     echo "AOK"
 else
     echo "nenene"

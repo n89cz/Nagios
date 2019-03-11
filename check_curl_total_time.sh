4defe9503cebf0f14bafe143d14c1ec868be1caa
Martin Dusek <martin.dusek@wedos.org>
2019-03-11 15:10:23 +0100

    curl check total response time

--- check_curl_total_time.sh
+++ check_curl_total_time.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-#DOMAIN=$1
-DOMAIN="https://domain.name"
+DOMAIN=$1
 
-#
-WARN=0.7
+#curl limits
+CURL_WARN=0.7
+CURL_CRIT=2
 
 #Nagios status codes
-STATE_OK="0"
-STATE_WARNING="1"
-STATE_CRITICAL="2"
-STATE_UNKNOWN="3"
-
-#RESPONSE=`curl -o /dev/null -s -w '%{time_total}' $DOMAIN`
+NAGI_OK="0"
+NAGI_WARNING="1"
+NAGI_CRITICAL="2"
+NAGI_UNKNOWN="3"
 
 RESPONSE=`curl -o /dev/null -s -w '%{time_total}' $DOMAIN | tr ',' '.'`
 
-if [[ "$RESPONSE" < "$WARN" ]]; then
-    echo "AOK"
+if [[ "$RESPONSE" < "$CURL_WARN" ]]; then
+    echo "OK - response time: $RESPONSE s"
+    exit $NAGI_OK
+elif [[ "$RESPONSE" > "$CURL_WARN" && "$RESPONSE" < $CURL_CRIT ]]; then
+    echo "Warning - response time: $RESPONSE s"
+    exit $NAGI_WARNING
+elif [[ "$RESPONSE" > "$CURL_CRIT" ]]; then
+    echo "Failure - response time: $RESPONSE s"
+    exit $NAGI_CRITICAL
 else
-    echo "nenene"
+    echo "Unknown status - $RESPONSE"
+    exit $NAGI_UNKNOWN
 fi
-
-#echo $RESPONSE | tr ',' '.'
-
-echo $RESPONSE
